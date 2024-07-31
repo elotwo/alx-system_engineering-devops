f5feb35be3bb13d545d3dba7fc1b3853b5ed5cf8
elotwo <obiagwu94@gmail.com>
2024-07-31 17:49:05 +0100

    me

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 import requests
 import sys
-def fetch(employe_id):
-    usr_url = f'https://jsonplaceholder.typicode.com/todos/{employe_id}'
+def fetch(employee_id):
+    usr_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
     response = requests.get(usr_url)
     if response.status_code == 200:
         data = response.json()
-        EMPLOYEE_NAME = data.get('name')
-        NUMBER_OF_DONE_TASKS = data.get('done_task')
-        total_task = data.get('total_task')
+        EMPLOYEE_NAME = data.get('userid')
+        NUMBER_OF_DONE_TASKS = [task for task in data if task.get('completed')]
+        total_task = len(data)
         print (f"Employee {EMPLOYEE_NAME} is done with tasks({len(NUMBER_OF_DONE_TASKS)}/{total_task}):")
         for task in NUMBER_OF_DONE_TASKS:
-            print (f"\t{task['title']}")
+            print (f"\t{task.get('title')}")
     else:
         print("failed to retrived data")
 
@@ -19,5 +19,5 @@ if __name__ == "__main__":
     if len (sys.argv) != 2:
         print("ERROR NO id")
         sys.exit(1)
-    employe_id = sys.argv[1]
-    fetch(employe_id)       
+    employee_id = sys.argv[1]
+fetch(employee_id)       
