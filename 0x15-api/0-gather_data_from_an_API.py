78be9635d517755321e3f63aae318196db7d66ce
elotwo <obiagwu94@gmail.com>
2024-07-31 18:54:54 +0100

    me

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,23 +1,49 @@
 #!/usr/bin/python3
+"""
+this script is about retriving data from API end point
+"""
 import requests
 import sys
+
+
 def fetch(employee_id):
-    usr_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
-    response = requests.get(usr_url)
-    if response.status_code == 200:
-        data = response.json()
-        EMPLOYEE_NAME = data.get('userid')
-        NUMBER_OF_DONE_TASKS = [task for task in data if task.get('completed')]
-        total_task = len(data)
-        print (f"Employee {EMPLOYEE_NAME} is done with tasks({len(NUMBER_OF_DONE_TASKS)}/{total_task}):")
-        for task in NUMBER_OF_DONE_TASKS:
-            print (f"\t{task.get('title')}")
-    else:
-        print("failed to retrived data")
+    user_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
+    user_response = requests.get(user_url)
+
+    if user_response.status_code != 200:
+        print("Failed to retrieve user data")
+        sys.exit(1)
+
+    user_data = user_response.json()
+    employee_name = user_data.get('name')
+    todos_url =
+    f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos'
+    todos_response = requests.get(todos_url)
+
+    if todos_response.status_code != 200:
+        print("Failed to retrieve TODO data")
+        sys.exit(1)
+
+    todos = todos_response.json()
+    total_tasks = len(todos) if todos else 0
+    done_tasks = [todo for todo in todos if todo.get('completed')]
+    print(
+            f"Employee {employee_name} is done with tasks("
+            f"{len(done_tasks)}/{total_tasks}):"
+    )
+    for task in done_tasks:
+        print(f"\t {task.get('title')}")
+
 
 if __name__ == "__main__":
-    if len (sys.argv) != 2:
-        print("ERROR NO id")
+    if len(sys.argv) != 2:
+        print("Usage: python3 script.py <employee_id>")
         sys.exit(1)
-    employee_id = sys.argv[1]
-fetch(employee_id)       
+
+    try:
+        employee_id = int(sys.argv[1])
+    except ValueError:
+        print("Employee ID must be an integer.")
+        sys.exit(1)
+
+    fetch(employee_id)
