85473b21261ddf1b3a45561b583d9a3b41ad829f
elotwo <obiagwu94@gmail.com>
2024-09-25 23:51:17 +0100

    me

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+import requests
+
+
+def top_ten(subreddit):
+    """Queries the Reddit API and prints the titles of
+    the first 10 hot posts for a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit to query.
+
+    Returns:
+        None: Prints None if the subreddit is invalid.
+    """
+    headers = {'User-Agent': 'my-reddit-top-ten/0.1'}
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        posts = response.json().get('data', {}).get('children', [])
+        for post in posts:
+            print(post['data']['title'])
+    else:
+        print(None)
