4228c5ec6f6399bc6820c57a00d5c03b6399c84e
elotwo <obiagwu94@gmail.com>
2024-09-26 15:04:28 +0100

    me

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,9 @@
-# this script fix the 500 error automate it using puppet
+# Ensure apache2 package is installed
+package { 'apache2':
+  ensure => installed,
+}
+
+# Ensure that the required directory exists under /var/www/html
 file { '/var/www/html/some_directory':
   ensure  => 'directory',
   owner   => 'www-data',
@@ -7,8 +12,18 @@ file { '/var/www/html/some_directory':
   require => Package['apache2'],
 }
 
+# Ensure Apache service is running and enabled
 service { 'apache2':
-  ensure    => 'running',
-  enable    => true,
-  subscribe => File['/var/www/html/some_directory'],
+  ensure  => running,
+  enable  => true,
+  require => Package['apache2'],
 }
+
+# Ensure proper ownership and permissions of the web root
+exec { 'fix-apache-permissions':
+  command => 'chown www-data:www-data /var/www/html/ -R && chmod 755 /var/www/html/ -R',
+  path    => ['/bin', '/usr/bin'],
+  onlyif  => 'find /var/www/html/ ! -user www-data',
+  require => [File['/var/www/html/some_directory'], Package['apache2']],
+}
+
