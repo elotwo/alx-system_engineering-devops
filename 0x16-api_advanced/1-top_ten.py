ed7b77a24b855023a65de347b53c91f4259a7acb
elotwo <obiagwu94@gmail.com>
2024-09-26 14:41:43 +0100

    me

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -15,13 +15,18 @@ def top_ten(subreddit):
     Returns:
         None: Prints None if the subreddit is invalid.
     """
-    headers = {'User-Agent': 'my-reddit-top-ten/0.1'}
+    headers = {'User-Agent': 'Mozilla/5.0'}
     url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
-    if response.status_code == 200:
-        posts = response.json().get('data', {}).get('children', [])
-        for post in posts:
-            print(post['data']['title'])
-    else:
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json().get('data', {}).get('children', [])
+            if not data:
+                print(None)
+                return
+            for post in data:
+                print(post.get('data', {}).get('title'))
+        else:
+            print(None)
+    except requests.exceptions.RequestException as e:
         print(None)
