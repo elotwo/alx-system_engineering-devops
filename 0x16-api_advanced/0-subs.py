00f4de2f0f795dff59dbb8c03ad6bd27ee41b414
elotwo <obiagwu94@gmail.com>
2024-09-26 12:17:44 +0100

    me

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,13 +9,10 @@ def number_of_subscribers(subreddit):
     """
     for record
     """
-    if subreddit is None or not isinstance(subreddit, str):
-        return 0
-    headers = {'User-Agent': 'my-reddit-subscriber-counter/0.1'}
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    headers = {"User-agent": "request"}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
-        data = response.json().get('data', {})
-        return data.get('subscribers', 0)
-    else:
-        return 0
+        data = response.json().get("data", {})
+        return data.get("subscribers", 0)
+    return 0
