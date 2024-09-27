bf59c0458382e37e2eaf97f9765c140493b07dfe
elotwo <obiagwu94@gmail.com>
2024-09-27 01:58:48 +0100

    me

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -16,16 +16,22 @@ file { '/var/www/html':
 
 # Ensure the index.html file exists and has the correct permissions
 file { '/var/www/html/index.html':
-  ensure  => present,
+  ensure  => file,
   owner   => 'www-data',
   group   => 'www-data',
   mode    => '0644',
-  content => '<title>Holberton &#8211; Just another WordPress site</title>
-<link rel="alternate" type="application/rss+xml" title="Holberton &raquo; Feed" href="http://127.0.0.1/?feed=rss2" />
-<link rel="alternate" type="application/rss+xml" title="Holberton &raquo; Comments Feed" href="http://127.0.0.1/?feed=comments-rss2" />
-        <div id="wp-custom-header" class="wp-custom-header"><img src="http://127.0.0.1/wp-content/themes/twentyseventeen/assets/images/header.jpg" width="2000" height="1200" alt="Holberton" /></div>  </div>
-                            <h1 class="site-title"><a href="http://127.0.0.1/" rel="home">Holberton</a></h1>
-        <p>Yet another bug by a Holberton student</p>',
+  content => '<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Holberton - Just another WordPress site</title>
+</head>
+<body>
+    <h1 class="site-title"><a href="http://127.0.0.1/" rel="home">Holberton</a></h1>
+    <p>Yet another bug by a Holberton student</p>
+</body>
+</html>',
   require => File['/var/www/html'],
 }
 
@@ -34,5 +40,78 @@ service { 'apache2':
   ensure  => running,
   enable  => true,
   require => [Package['apache2'], File['/var/www/html/index.html']],
+}
+
+# Validate Apache configuration
+exec { 'validate-apache-config':
+  command => '/usr/sbin/apachectl configtest',
+  path    => ['/usr/sbin', '/bin'],
+  refreshonly => true,
+  subscribe => Service['apache2'],
+}
+
+# Restart Apache if the configuration is changed
+exec { 'restart-apache':
+  command     => '/bin/systemctl restart apache2',
+  refreshonly => true,
+  subscribe    => Exec['validate-apache-config'],
+}
+# This Puppet script fixes file permissions for Apache and ensures the Apache service is running properly.
+
+# Ensure apache2 package is installed
+package { 'apache2':
+  ensure => installed,
+}
+
+# Ensure the web root directory exists with correct permissions
+file { '/var/www/html':
+  ensure  => directory,
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0755',
+  require => Package['apache2'],
+}
+
+# Ensure the index.html file exists and has the correct permissions
+file { '/var/www/html/index.html':
+  ensure  => file,
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0644',
+  content => '<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Holberton - Just another WordPress site</title>
+</head>
+<body>
+    <h1 class="site-title"><a href="http://127.0.0.1/" rel="home">Holberton</a></h1>
+    <p>Yet another bug by a Holberton student</p>
+</body>
+</html>',
+  require => File['/var/www/html'],
+}
+
+# Ensure Apache service is running and enabled
+service { 'apache2':
+  ensure  => running,
+  enable  => true,
+  require => [Package['apache2'], File['/var/www/html/index.html']],
+}
+
+# Validate Apache configuration
+exec { 'validate-apache-config':
+  command => '/usr/sbin/apachectl configtest',
+  path    => ['/usr/sbin', '/bin'],
+  refreshonly => true,
+  subscribe => Service['apache2'],
+}
+
+# Restart Apache if the configuration is changed
+exec { 'restart-apache':
+  command     => '/bin/systemctl restart apache2',
+  refreshonly => true,
+  subscribe    => Exec['validate-apache-config'],
 }
 
