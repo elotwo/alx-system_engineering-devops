48e2e9111fe6c9f9e4122d391fd64de0fb744430
elotwo <obiagwu94@gmail.com>
2024-09-27 12:16:10 +0100

    me

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,11 +10,20 @@ def number_of_subscribers(subreddit):
     for record
     """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
+    headers = {
+            "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+            }
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        response.raise_for_status()
+        results = response.json().get("data")
+        if results:
+            return results.get("subscribers", 0)
+        else:
+            return 0
+    except requests.exceptions.HTTPError as http_err:
+        print(f"HTTP error occurred: {http_err}")
+        return 0
+    except requests.exceptions.RequestException as req_err:
+        print(f"Error occurred: {req_err}")
         return 0
