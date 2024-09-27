394fcb4f0f7768316f6d862890418488c6405b58
elotwo <obiagwu94@gmail.com>
2024-09-27 01:49:02 +0100

    me

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -20,7 +20,12 @@ file { '/var/www/html/index.html':
   owner   => 'www-data',
   group   => 'www-data',
   mode    => '0644',
-  content => '<html><head><title>Holberton</title></head><body>Yet another bug by a Holberton student</body></html>',
+  content => '<title>Holberton &#8211; Just another WordPress site</title>
+<link rel="alternate" type="application/rss+xml" title="Holberton &raquo; Feed" href="http://127.0.0.1/?feed=rss2" />
+<link rel="alternate" type="application/rss+xml" title="Holberton &raquo; Comments Feed" href="http://127.0.0.1/?feed=comments-rss2" />
+        <div id="wp-custom-header" class="wp-custom-header"><img src="http://127.0.0.1/wp-content/themes/twentyseventeen/assets/images/header.jpg" width="2000" height="1200" alt="Holberton" /></div>  </div>
+                            <h1 class="site-title"><a href="http://127.0.0.1/" rel="home">Holberton</a></h1>
+        <p>Yet another bug by a Holberton student</p>',
   require => File['/var/www/html'],
 }
 
