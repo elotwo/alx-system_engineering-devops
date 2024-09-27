369989267e5ec1cc333127353c88ebc3c0a2d0e5
elotwo <obiagwu94@gmail.com>
2024-09-27 01:29:13 +0100

    me

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,15 +1,9 @@
-# Apache Web Server Configuration
-
+# Ensure apache2 package is installed
 package { 'apache2':
   ensure => installed,
 }
 
-service { 'apache2':
-  ensure  => running,
-  enable  => true,
-  require => Package['apache2'],
-}
-
+# Ensure the /var/www/html directory has the correct ownership and permissions
 file { '/var/www/html':
   ensure  => 'directory',
   owner   => 'www-data',
@@ -18,19 +12,28 @@ file { '/var/www/html':
   require => Package['apache2'],
 }
 
+# Ensure that an index.html file exists with correct content and permissions
 file { '/var/www/html/index.html':
   ensure  => present,
-  content => '<html>Hello World!</html>',
+  content => '<html><h1>Holberton</h1></html>',
   owner   => 'www-data',
   group   => 'www-data',
   mode    => '0644',
   require => File['/var/www/html'],
 }
 
+# Ensure Apache service is running and enabled
+service { 'apache2':
+  ensure  => running,
+  enable  => true,
+  require => Package['apache2'],
+}
+
+# Ensure correct permissions of the web root
 exec { 'fix-apache-permissions':
-  command     => 'chown -R www-data:www-data /var/www/html && chmod -R 755 /var/www/html',
-  path        => ['/bin', '/usr/bin'],
-  onlyif      => 'find /var/www/html/ ! -user www-data',
-  require     => File['/var/www/html/index.html'],
+  command => 'chown -R www-data:www-data /var/www/html && chmod -R 755 /var/www/html',
+  path    => ['/bin', '/usr/bin'],
+  onlyif  => 'find /var/www/html/ ! -user www-data',
+  require => File['/var/www/html/index.html'],
 }
 
